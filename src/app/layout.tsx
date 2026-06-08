85ba6d35302598328231220656bc986de5267bda
hitJangra <jangrahitender12@gmail.com>
2026-06-08 09:01:24 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Merienda, Tiro_Devanagari_Hindi } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const merienda = Merienda({
   subsets: ["latin"],
